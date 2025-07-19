3cdac6bd9f2333d21b48ef30b59fbc03b0c16e60
Your Name <biswastatay73@gmail.com>
2025-07-19 15:16:22 +0530

    feat: Implement game over and reset functionality
    This commit introduces the following features:
    
    - **Game Over Dialog:**
        - Displays an "Game Over" dialog when the game ends.
        - Provides a "Play Again" button to restart the game.
    - **Game Reset:**
        - Resets the bird's position and velocity.
        - Resets the game over state.
        - Resumes the game engine.
    - **Constants:**
        - Uses `birdStartY` from `constants.dart` for resetting the bird's initial Y position.

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:fluppy_bird/components/bird.dart';
+import 'package:fluppy_bird/constants/constants.dart';
 import 'package:fluppy_bird/pages/background.dart';
 import 'package:flutter/material.dart';
 
@@ -42,5 +43,33 @@ class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
 
     isGameOver = true;
     pauseEngine();
+
+    //Show Dialog box
+
+    showDialog(
+      context: buildContext!,
+      builder: (context) => AlertDialog(
+        title: const Text("Game Over"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              //pop the game
+              Navigator.pop(context);
+
+              //reset game
+              resetGame();
+            },
+            child: const Text("Play Again"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void resetGame() {
+    bird.position = Vector2(birdStartY, birdStartY);
+    bird.velocity = 0;
+    isGameOver =false;
+    resumeEngine();
   }
 }
